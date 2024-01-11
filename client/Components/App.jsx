4922260678b385c806bd03c9be1c95d07c558872
Scott Zembsch <scottzembsch@gmail.com>
2024-01-10 19:35:36 -0800

    server routes added and some db structure

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -3,16 +3,18 @@ import { Route, Routes, Link } from "react-router-dom";
 // import components
 
 // import style
-
+<h1>Root page</h1>  
 
 function App() {
 
   return (
 
       <Routes>
-        <Route path="/" element={<h1>Root page</h1>} />
+        <Route path="/" element={<h1>sign up page</h1>} />
         <Route path="/signup" element={<h1>sign up page</h1>} />
+        <Route path="/signin" element={<h1>sign in page</h1>} />
         <Route path="/home" element={<h1>home page</h1>} />
+        <Route path="/addtrip" element={<h1>add trip page</h1>} />
       </Routes>
 
   )
