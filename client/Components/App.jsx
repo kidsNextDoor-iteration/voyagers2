9bd88dcb13a189024fef584b942f2bed9e4d8f8c
George Huang <gzh@princeton.alumni.com>
2024-01-10 17:41:13 -0800

    Basic signup page created

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
 // import components
 import Login from "./Login"
+import Signup from "./Signup"
 // import style
 
 
@@ -11,7 +12,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<h1>Root page</h1>} />
-        <Route path="/signup" element={<h1>sign up page</h1>} />
+        <Route path="/signup" element={<Signup/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/home" element={<h1>home page</h1>} />
       </Routes>
