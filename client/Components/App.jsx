312bb276dd567e7808004df3a19fc11a7b13adc8
George Huang <gzh@princeton.alumni.com>
2024-01-10 17:29:01 -0800

    Basic login page with form created

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
 // import components
-
+import Login from "./Login"
 // import style
 
 
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Root page</h1>} />
         <Route path="/signup" element={<h1>sign up page</h1>} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/home" element={<h1>home page</h1>} />
       </Routes>
 
