3df693a62f3cb19821328399f189db7a73f49612
Alex <Alex.D.Dawkins@gmail.com>
2024-01-10 23:06:26 -0500

    add basic homepage structure, add homepage element to App.jsx

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
 // import components
+import HomePage from "./homePage.jsx";
 
 // import style
 
@@ -10,7 +11,7 @@ function App() {
   return (
 
       <Routes>
-        <Route path="/" element={<h1>Root page</h1>} />
+        <Route path="/" element={HomePage} />
         <Route path="/signup" element={<h1>sign up page</h1>} />
         <Route path="/home" element={<h1>home page</h1>} />
       </Routes>
@@ -18,4 +19,4 @@ function App() {
   )
 }
  
-export default App;
+export default App();
