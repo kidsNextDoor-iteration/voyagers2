b66dbfede9a5c0e161f4b18fb7995f16313c1078
Scott Zembsch <scottzembsch@gmail.com>
2024-01-12 11:06:06 -0800

    got working image upload to s3

--- client/Components/ImageUpload.jsx
+++ client/Components/ImageUpload.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 function ImageUpload() {
+
+  const [file, setFile] = useState();
+  const [caption, setCaption] = useState('');
+
+  // const submit = async event => {
+  //   event.preventDefault()
+
+  //   const formData = new FormData();
+  //   formData.append("image", file)
+  //   formData.append("caption", caption)
+  //   await axios.post("/api/uploadimage", formData, { headers: {'Content-Type': 'multipart/form-data'}})
+  // }
+
+  const submit = async event => {
+    event.preventDefault();
+  
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("caption", caption);
+  
+    try {
+      const response = await fetch("/api/uploadimage", {
+        method: "POST",
+        body: formData,
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const responseData = await response.json();
+      console.log("Upload successful:", responseData);
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  };
+  
+
+
   return (
     <>
       <h1>image upload</h1>
-      <input type="file" />
-      <input type="text" />
+        {/* <form action="/api/uploadimage" method='post' enctype="multipart/form-data">
+        <input type="file" name='image' accept='image/*' />
+        <input type="text" name='caption' placeholder='caption' />
+        <input type="submit" value="Submit"></input>
+      </form> */}
+
+      <form onSubmit={submit}>
+        <input onChange={e => setFile(e.target.files[0])} type="file" accept='image/*'></input>
+        <input value={caption} onChange={e => setCaption(e.target.value)} type='text' placeholder='caption' />
+        <button type='submit'>Submit</button>
+      </form>
+
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -5,10 +5,35 @@ const PORT = 3000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+require('dotenv').config();
+
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+console.log('bucketname: ',bucketName)
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
+  },
+  region: bucketRegion
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
@@ -30,6 +55,31 @@ app.post('/signup',
     res.status(200).send('request to signup successful')
   }
 )
+
+// ------------------- UPLOAD IMAGE ------------------ //
+app.post('/api/uploadimage', 
+  upload.single('image'),
+  async (req, res) => {
+  console.log("req.body: ", req.body)
+  console.log('req.file: ', req.file)
+
+  req.file.buffer
+
+  const params = {
+    Bucket: bucketName,
+    Key: req.file.originalname,
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype,
+  }
+  const command = new PutObjectCommand(params)
+
+  await s3.send(command)
+
+  res.send({})
+})
+
+
+
 
 
 // ----------- DB ROUTING ------------------- //
@@ -62,6 +112,11 @@ app.get('/imageDemo', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`);
 });
+
