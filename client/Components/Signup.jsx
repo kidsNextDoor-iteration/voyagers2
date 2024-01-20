3a36c50d59dd6c338a265aec01faa0c10f191838
Scott Zembsch <scottzembsch@gmail.com>
2024-01-20 09:17:18 -0800

    added landing/singup/signin images

--- client/Components/Signup.jsx
+++ client/Components/Signup.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "../stylesheets/images/background-1.jpg"
+import newImg from '../Images/pexels-ricky-esquivel-1563256.jpg';
 import { Link } from 'react-router-dom';
+
 
 const Signup = () => {
     return (
@@ -10,10 +12,11 @@ const Signup = () => {
             </nav>
             <div className="content">
                 <div className="image">
-                    <img src={Image}></img>
+                    <img src={newImg}></img>
                 </div>
                 <div className="form">
                     <form action="/signup" method="POST">
+                    <h1>Lets Take A Trip..</h1>
                         <input name="firstName" type="text" placeholder="First Name"></input>
                         <input name="lastName" type="text" placeholder="Last Name"></input>
                         <input name="email" type="text" placeholder="Email"></input>
