698446932ef4983ae4eed2986f82c18205b33f2c
Ross Terker <ross.terker@gmail.com>
2024-02-03 09:14:07 -0500

    added testing file

--- __tests__/server-testing.js
+++ __tests__/server-testing.js
@@ -45,52 +45,52 @@ describe('Server Route Testing via Supertest', () => {
 
     });
 
-    describe('Internal Routing Testing', () => {
+    const passingBody = {
+        firstname: 'testUser',
+        lastname: 'testUserLastName',
+        email: 'testUserEmail@test.com',
+        password: 'P@ssword1!' 
+    };
+    const missingFirstNameBody = {
+        lastname: 'testUserLastName',
+        email: 'testUserEmail@test.com',
+        password: 'P@ssword1!' 
+    };
+    const missingLastNameBody = {
+        firstname: 'testUser',
+        email: 'testUserEmail@test.com',
+        password: 'P@ssword1!' 
+    };
 
-        const passingBody = {
-            firstname: 'testUser',
-            lastname: 'testUserLastName',
-            email: 'testUserEmail@test.com',
-            password: 'P@ssword1!' 
-        };
-        const missingFirstNameBody = {
-            lastname: 'testUserLastName',
-            email: 'testUserEmail@test.com',
-            password: 'P@ssword1!' 
-        };
-        const missingLastNameBody = {
-            firstname: 'testUser',
-            email: 'testUserEmail@test.com',
-            password: 'P@ssword1!' 
-        };
+    let goodResponse;
+    let goodDbResult;
+    let testEmail;
+    let testFirstName;
+    let testLastName;
+    beforeAll(async () => {
+        goodResponse = await request(server).post('/internal/signup').send(passingBody);
 
-        let goodResponse;
-        let goodDbResult;
-        let testEmail;
-        let testFirstName;
-        let testLastName;
-        beforeAll(async () => {
-            goodResponse = await request(server).post('/internal/signup').send(passingBody);
+        testEmail = passingBody.email;
+        testFirstName = passingBody.firstname;
+        testLastName = passingBody.lastname;
+        let values = [testEmail];
+        let queryString = `
+        SELECT * FROM users WHERE email = $1;
+        `;
+        goodDbResult = await db.query(queryString, values);
+    });
 
-            testEmail = passingBody.email;
-            testFirstName = passingBody.firstname;
-            testLastName = passingBody.lastname;
-            let values = [testEmail];
-            let queryString = `
-            SELECT * FROM users WHERE email = $1;
-            `;
-            goodDbResult = await db.query(queryString, values);
-        });
+    afterAll(async () => {
+        const values = [passingBody.email];
+        const queryString = `
+        DELETE FROM users WHERE email = $1;
+        `;
+        db.query(queryString, values)
+        //close pool after
+        .then(() => db.end());
+    });
 
-        afterAll(async () => {
-            const values = [passingBody.email];
-            const queryString = `
-            DELETE FROM users WHERE email = $1;
-            `;
-            db.query(queryString, values)
-            //close pool after
-            .then(() => db.end());
-        });
+    describe('Internal Route Testing', () => {
 
         describe("test: app.post('/internal/signup')", () => {
 
@@ -188,36 +188,220 @@ describe('Server Route Testing via Supertest', () => {
 
         });
 
-    })
-
-
-
+    });
 
 
     describe('Trip Routing Testing', () => {
 
-        describe("test: app.post('/addTrips')", () => {
+        const startDate = new Date();
+        const endDate = new Date ();
+        //make end date 1 month later
+        endDate.setMonth(endDate.getMonth() + 1);
+
+        const validTrip = {
+            city: 'test city',
+            brand: 'test brand',
+            description: 'test description',
+            startDate: startDate.toLocaleDateString(),
+            endDate: endDate.toLocaleDateString(),
+            idea: 'test idea'
+        };
+
+        let response;
+        let testTripId;
+        beforeAll(async () => {
+            response = await request(server).post('/trip/addTrip').set('Cookie', [`userid=${goodDbResult.rows[0].userid}`]).send(validTrip);
+            testTripId = response.body[0].tripid;
+        });
+
+        //deleting the added test trip where we test the delete trip route below
+
+        describe("test: app.post('/trip/addTrip')", () => {
+
+            it('should return 200 status code', () => {
+                expect(response.status).toBe(200);
+            });
+
+            it('should return added trip', () => {
+                //date comes back from db as Z strings, so convert back to same format as input. this will maintain the actual date for valid comparison
+                const responseStartDate = new Date(response.body[0].startdate);
+                const responseEndDate = new Date(response.body[0].enddate);
+                const validResponse = {...response.body[0], startdate: responseStartDate.toLocaleDateString(), enddate: responseEndDate.toLocaleDateString()};
+                //passing in startDate and endDate in camel case, but db returns without camel case. removing camel case from input keys to match db structure.
+                const validInput = {
+                    city: validTrip.city,
+                    brand: validTrip.brand,
+                    description: validTrip.description,
+                    startdate: validTrip.startDate,
+                    enddate: validTrip.endDate,
+                    idea: validTrip.idea
+                };
+                expect(validResponse).toEqual(expect.objectContaining(validInput));
+            });
+
+            it('should only return array with one added trip', () => {
+                expect(response.body.length).toBe(1);
+            });
+            
+
+        });
+
+        describe("test: app.get('/trip/getTrips')", () => {
+
+            let response;
+            // let dbResponse;
+            beforeAll(async () => {
+                // const values = [goodDbResult.rows[0].userid];
+                // const queryString = `
+                // SELECT userid, tripId, startdate, enddate, city, brand, description, idea, status FROM trips WHERE userId = $1
+                // `;
+                // dbResponse = await db.query(queryString, values);
+
+                response = await request(server).get('/trip/getTrips').set('Cookie', [`userid=${goodDbResult.rows[0].userid}`]);
+            });
+
+            it('should return 200 status code', () => {
+                expect(response.statusCode).toBe(200);
+            });
+
+            it('should only return trips for that user', () => {
+                //if at some point userid is no longer returned with the /trip/getTrips request, this test will need to be adjusted
+                response.body.forEach((trip => {
+                    expect(trip.userid).toEqual(goodDbResult.rows[0].userid);
+                }));
+            });
+
+            it('should return array', () => {
+                expect(Array.isArray(response.body)).toBe(true);
+            });
+
+        });
+
+        describe("test: app.patch('/trip/editTrip')", () => {
+
+            const startDate = new Date();
+            //set startDate to 6 months later
+            startDate.setMonth(startDate.getMonth() + 6);
+            const endDate = new Date ();
+            //set endDate to 1 year later
+            endDate.setFullYear(endDate.getFullYear() + 1);
+
+            let editDetails = {
+                startDate: startDate.toLocaleDateString(),
+                endDate: endDate.toLocaleDateString(),
+                city: 'Philadelphia',
+                brand: 'testEditBrand',
+                description: 'testEditDescription',
+                idea: 'damnGoodEditIdea',
+                status: 'editStatus'
+            };
+
+            let response;
+            // let dbResponse;
+            beforeAll(async () => {
+                // const values = [testTripId];
+                // const queryString = `
+                // SELECT userid, tripId, startDate, endDate, city, brand, description, idea, status FROM trips WHERE tripId = $1
+                // `;
+                // dbResponse = await db.query(queryString, values);
+
+                response = await request(server).patch(`/trip/editTrip?tripId=${testTripId}`).send(editDetails);
+            });
+
+            it('should return 200 status code', () => {
+                expect(response.statusCode).toBe(200);
+            });
+
+            it('should return 1 trip', () => {
+                expect(response.body.length).toBe(1);
+            });
+
+            it('should return edited trip', () => {
+                 //date comes back from db as Z strings, so convert back to same format as input. this will maintain the actual date for valid comparison
+                const validResponse = {...response.body[0], startdate: new Date(response.body[0].startdate).toLocaleDateString(), enddate: new Date(response.body[0].enddate).toLocaleDateString()};
+                //passing in startDate and endDate in camel case, but db returns without camel case. removing camel case from input keys to match db structure.
+                editDetails = {
+                    startdate: editDetails.startDate,
+                    enddate: editDetails.endDate,
+                    city: editDetails.city,
+                    brand: editDetails.brand,
+                    description: editDetails.description,
+                    idea: editDetails.idea,
+                    status: editDetails.status
+                };
+                expect(validResponse).toEqual(expect.objectContaining(editDetails));
+            });
+
+            it('should return array', () => {
+                expect(Array.isArray(response.body)).toBe(true);
+            });
 
 
         });
 
-        describe("test: app.get('/getTrips')", () => {
+        describe("test: app.get('/trip/getTripDetails')", () => {
 
+            let response;
+            // let dbResponse;
+            beforeAll(async () => {
+                // const values = [testTripId];
+                // const queryString = `
+                // SELECT startDate, endDate, city, brand, description, idea, status FROM trips WHERE tripId = $1
+                // `;
+                // dbResponse = await db.query(queryString, values);
 
+                response = await request(server).get(`/trip/getTripDetails?tripId=${testTripId}`);
+            });
+
+            it('should return 200 status code', () => {
+                expect(response.statusCode).toBe(200);
+            });
+
+            //TODO: check that details are for correct tripId; however, right now the response doesn't seem to contain the tripId. front end seems
+            //to be using the tripId, but I can't find where it's pulling it from
+            it('should return correct trip and details', () => {
+                expect(Object.keys(response.body[0])).toEqual(['startdate', 'enddate', 'city', 'brand', 'description', 'idea', 'status']);
+            });
+
+            it('should return 1 trip', () => {
+                expect(response.body.length).toBe(1);
+            });
+
+            it('should set tripId cookie', () => {
+                //taking just the tripId part of the cookie. stripping the other cookie details.
+                let responseCookie = response.res.headers['set-cookie'][0];
+                responseCookie = responseCookie.split(';')[0];
+                expect(responseCookie).toBe(`tripId=${testTripId}`); 
+            })
+
+            it('should return array', () => {
+                expect(Array.isArray(response.body)).toBe(true);
+            });
         });
 
-        describe("test: app.patch('/editTrip')", () => {
+        describe("test: app.delete('/trip/deleteTrip')", () => {
 
+            let response;
+            let dbResponse;
+            beforeAll(async () => {
+                response = await request(server).delete(`/trip/deleteTrip?tripId=${testTripId}`);
 
-        });
+                const values = [testTripId];
+                const queryString = `
+                SELECT * FROM trips WHERE tripId = $1
+                `;
+                dbResponse = await db.query(queryString, values);
+            });
 
-        describe("test: app.get('/getTripDetails')", () => {
+            it('should return 200 status code', () => {
+                expect(response.statusCode).toBe(200);
+            });
 
+            it('should delete trip from db', () => {
+                expect(dbResponse.rows.length).toBe(0);
+            });
 
-        });
-
-        describe("test: app.delete('/deleteTrip')", () => {
-
+            //TODO: should return deleted trip??? doesn't seem to do this now, but we should add it
 
         });
 
